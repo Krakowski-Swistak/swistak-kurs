65579a682b95c30bdd2856ea852857d360061f2d
Kamil <k.medrala99@gmail.com>
2024-06-20 13:52:42 +0200

    enrolled coursees shortcode test

--- theme/functions.php
+++ theme/functions.php
@@ -243,7 +243,7 @@ function shortcode_tutor_enrolled_course( $atts ) {
 
 <h3><?php _e('Enrolled Courses', 'tutor'); ?></h3>
 
-<div class="tutor-dashboard-content-inner">
+<div class="tutor-dashboard-content-inner flex flex-wrap gap-5">
 
 	<?php
 	$my_courses = tutor_utils()->get_enrolled_courses_by_user(get_current_user_id(), array('private', 'publish'));
@@ -251,7 +251,6 @@ function shortcode_tutor_enrolled_course( $atts ) {
 	if ($my_courses && $my_courses->have_posts()):
 		while ($my_courses->have_posts()):
 			$my_courses->the_post();
-			$avg_rating = tutor_utils()->get_course_rating()->rating_avg;
 			$tutor_course_img = get_tutor_course_thumbnail_src();
             /**
              * wp 5.7.1 showing plain permalink for private post
@@ -262,40 +261,26 @@ function shortcode_tutor_enrolled_course( $atts ) {
             $post = $my_courses->post;
             $custom_url = home_url($post->post_type.'/'.$post->post_name);
 			?>
-            <div class="tutor-mycourse-wrap tutor-mycourse-<?php the_ID(); ?>">
-                <a class="tutor-stretched-link" href="<?php echo esc_url($custom_url);?>"><span class="sr-only"><?php the_title(); ?></span></a>
-                <div class="tutor-mycourse-thumbnail" style="background-image: url(<?php echo esc_url($tutor_course_img); ?>)"></div>
-                <div class="tutor-mycourse-content">
-                    <div class="tutor-mycourse-rating">
-		                <?php tutor_utils()->star_rating_generator($avg_rating); ?>
-                    </div>
-
-                    <h3><a href="<?php echo esc_url($custom_url);?>"><?php the_title(); ?></a></h3>
+            <a href="<?php echo esc_url($custom_url);?>" class="flex flex-col rounded-2xl border border-[#dcdfe5] max-w-96">
+                <img src="<?php echo esc_url($tutor_course_img); ?>" class="aspect-[4/3] object-cover w-full">
+                <div class="tutor-mycourse-content p-5 flex flex-col">
+                    <h3><?php the_title(); ?></h3>
                     
                     <div class="tutor-meta tutor-course-metadata">
 		                <?php
                             $total_lessons = tutor_utils()->get_lesson_count_by_course();
                             $completed_lessons = tutor_utils()->get_completed_lesson_count_by_course();
 		                ?>
-                        <ul>
-                            <li>
-				                <?php
-				                _e('Total Lessons:', 'tutor');
-				                echo "<span>$total_lessons</span>";
-				                ?>
-                            </li>
-                            <li>
-				                <?php
-				                _e('Completed Lessons:', 'tutor');
-				                echo "<span>$completed_lessons / $total_lessons</span>";
-				                ?>
-                            </li>
-                        </ul>
+                        <p>
+							<?php
+								_e('Completed Lessons:', 'tutor');
+								echo "<span>$completed_lessons / $total_lessons</span>";
+							?>
+						</p>
                     </div>
-	                <?php tutor_course_completing_progress_bar(); ?>
                 </div>
 
-            </div>
+            </a>
 
 			<?php
 		endwhile;
