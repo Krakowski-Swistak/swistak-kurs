7b6e926392e2bd4bd7015d76d446249e96a3a33c
Tomek <TK-54-4B>
2025-03-17 22:08:24 +0100

    single product page

--- theme/woocommerce/content-single-product.php
+++ theme/woocommerce/content-single-product.php
@@ -32,30 +32,33 @@ if (post_password_required()) {
 }
 ?>
 <div id="product-<?php the_ID(); ?>" <?php wc_product_class('', $product); ?>>
-	<div class="container mb-20 flex flex-col lg:flex-row lg:justify-between gap-5 md:gap-6 lg:gap-10">
-		<div class="w-full">
-			<div>
-				<?php the_title('<h1 class="mb-10 text-[32px] font-medium text-[#3B424F]">', '</h1>'); ?>
-				<div class="mb-16">
-					<?php swistak_kurs_post_thumbnail(); ?>
-				</div>
-				<div class="[&>h2]:mb-[16px] [&>h2]:text-[#22272F] [&>h2]:text-[21px] [&>h2]:font-medium [&>ul]:list-disc" style="li:marker:#132787;">
-					<?php the_content(); ?>
+	<div class="container mx-auto">
+		<div class="mb-20 flex flex-col lg:flex-row lg:justify-between gap-5 md:gap-6 lg:gap-10">
+			<div class="w-full">
+				<div>
+					<?php the_title('<h1 class="mb-10 text-[32px] font-medium text-[#3B424F]">', '</h1>'); ?>
+					<div class="mb-16">
+						<?php swistak_kurs_post_thumbnail(); ?>
+					</div>
+					<div class="font-light [&>h2]:mb-[16px] [&>h2]:text-[#22272F] [&>h2]:text-[21px] [&>h2]:font-medium [&>ul]:list-disc [&>ul]:ml-[18px]">
+						<?php the_content(); ?>
+					</div>
 				</div>
 			</div>
+			<aside class="lg:basis-1/3 md:grow-0 md:shrink-0 relative">
+				<div class="lg:sticky top-[115px]">
+					<p>Informacje</p>
+					<?php
+					$product = wc_get_product(get_the_ID());
+					echo "<a href='" . $product->add_to_cart_url() . "'>add to cart</a>";
+					?>
+					<div class="price-wrapper">
+						<?php echo $product->get_price_html(); ?>
+					</div>
+				</div>
+			</aside>
 		</div>
-		<aside class="lg:basis-1/3 md:grow-0 md:shrink-0 relative">
-			<div class="lg:sticky top-[115px]">
-				<p>Informacje</p>
-				<?php
-				$product = wc_get_product(get_the_ID());
-				echo "<a href='" . $product->add_to_cart_url() . "'>add to cart</a>";
-				?>
-				<div class="price-wrapper">
-					<?php echo $product->get_price_html(); ?>
-				</div>
-			</div>
-		</aside>
+		<a href="<?php echo get_home_url(); ?>" class="padding-[8px_16px] border-[1px] border-primary">POWRÓT DO STRONY GŁÓWNEJ</a>
 	</div>
 </div>
 
