82b9a689eadb19bf88063a739b13f7f0b1e26e59
Tomek <TK-54-4B>
2025-03-17 22:21:36 +0100

    single product page

--- theme/woocommerce/content-single-product.php
+++ theme/woocommerce/content-single-product.php
@@ -33,7 +33,7 @@ if (post_password_required()) {
 ?>
 <div id="product-<?php the_ID(); ?>" <?php wc_product_class('', $product); ?>>
 	<div class="container mx-auto">
-		<div class="mb-20 flex flex-col lg:flex-row lg:justify-between gap-5 md:gap-6 lg:gap-10">
+		<div class="mb-9 flex flex-col lg:flex-row lg:justify-between gap-5 md:gap-6 lg:gap-10">
 			<div class="w-full">
 				<div>
 					<?php the_title('<h1 class="mb-10 text-[32px] font-medium text-[#3B424F]">', '</h1>'); ?>
@@ -46,19 +46,25 @@ if (post_password_required()) {
 				</div>
 			</div>
 			<aside class="lg:basis-1/3 md:grow-0 md:shrink-0 relative">
-				<div class="lg:sticky top-[115px]">
-					<p>Informacje</p>
+				<div class="w-full p-6 border-[#DDE3EE] lg:sticky top-[96px] text-[#22272F]">
+					<p class="pb-6 border-b-[#DDE3EE] text-[21px]">Informacje:</p>
+					<div class="price-wrapper">
+						<?php echo $product->get_price_html(); ?>
+					</div>
+					<div class="type-wrapper flex justify-between">
+						<p class="text-[14px] font-medium"><span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+									<path d="M19 21H5C3.89543 21 3 20.1046 3 19V5C3 3.89543 3.89543 3 5 3H19C20.1046 3 21 3.89543 21 5V19C21 20.1046 20.1046 21 19 21ZM5 10V19H19V10H5ZM5 5V8H19V5H5Z" fill="#22272F" />
+								</svg></span>RODZAJ</p>
+						<p>Szkolenie online</p>
+					</div>
 					<?php
 					$product = wc_get_product(get_the_ID());
 					echo "<a href='" . $product->add_to_cart_url() . "'>add to cart</a>";
 					?>
-					<div class="price-wrapper">
-						<?php echo $product->get_price_html(); ?>
-					</div>
 				</div>
 			</aside>
 		</div>
-		<a href="<?php echo get_home_url(); ?>" class="p-[8px_16px] rounded-2xl border-[1px] border-primary hover:bg-primary hover:text-white transition duration-200">POWRÓT DO STRONY GŁÓWNEJ</a>
+		<a href="<?php echo get_home_url(); ?>" class="p-[8px_16px] rounded-2xl border-[1px] border-primary hover:bg-primary hover:text-white transition duration-300">POWRÓT DO STRONY GŁÓWNEJ</a>
 	</div>
 </div>
 
